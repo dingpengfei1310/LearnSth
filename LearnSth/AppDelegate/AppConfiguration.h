24ff38f4630a99bb7c29e9f7c680ea704fe52979
丁鹏飞 <dingpengfei@dingpengfeideMac-mini.local>
2016-10-27 11:21:09 +0800

    0

--- LearnSth/AppDelegate/AppConfiguration.h
+++ LearnSth/AppDelegate/AppConfiguration.h
@@ -20,6 +20,7 @@
 //# define NSLog(...)
 //#endif
 
+#pragma mark
 //#ifdef DEBUG
 //#define NSLog(FORMAT, ...) fprintf(stderr,"%s:%d\t%s\n",[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
 //#else
